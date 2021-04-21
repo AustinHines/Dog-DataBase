205573575fd655f070bbfc50918ed531ea63b223
Austin Hines <Austinhines0605@gmail.com>
2021-04-21 16:21:51 -0500

    Finished Header

---
+++ src/main-page/Main.js
@@ -0,0 +1,17 @@
+import './Main.css'
+
+const Landing = () => {
+    return (
+        <div>
+            <div className="heading">
+                <a href="index.html">Reporting Found Animal</a>
+                <a href="index.html">Found Animals</a>
+            </div>
+        </div>
+    )
+}
+
+export default Landing
+            
+                
+
