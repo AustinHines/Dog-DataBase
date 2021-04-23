74283fcf512d93c1d239443f1646e97b11b7363d
Austin Hines <Austinhines0605@gmail.com>
2021-04-22 20:40:48 -0500

    Added Tip section

--- src/main-page/Main.js
+++ src/main-page/Main.js
@@ -3,15 +3,40 @@ import './Main.css'
 const Landing = () => {
     return (
         <div>
-            <div className="heading">
-                <a href="index.html">Reporting Found Animal</a>
-                <a href="index.html">Found Animals</a>
+            
+                <div className="heading" data-aos="slide-down">
+
+                </div>
+
+            
+
+            <div className="main-body">
+                <div className="left-report" data-aos="fade-right">
+                    <h1>I'm Here to report a found animal</h1>
+                </div>
+                <div className="right-report" data-aos="fade-left">
+                    <h1>I want to look at found animals</h1>
+                </div>
+            </div>
+
+            <div className="tips" data-aos="fade-up">
+                <div className="find">
+
+                </div>
+
+                <div className="preventing">
+
+                </div>
+
+                <div className="basic">
+
+                </div>
             </div>
         </div>
     )
 }
 
 export default Landing
-            
-                
+
+
 
